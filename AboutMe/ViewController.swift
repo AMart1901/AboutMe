c23defc7726a070263236f58a2f45b6fd4bf1d1f
AMart1901 <armartin2300@gmail.com>
2019-04-23 09:28:32 -0700

    Initial Commit

---
+++ AboutMe/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AboutMe
+//
+//  Created by Lambda_School_Loaner_64 on 4/23/19.
+//  Copyright © 2019 Lambda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
